4ec14a5bfd5c50a68e57e3e5321c0e0b60aceb93
nathants <me@nathants.com>
2020-07-24 14:33:27 -0700

    install and test scripts

---
+++ example/run_on_aws.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -xeou pipefail
+
+if ! which aws-ec2-new; then
+    echo fatal: need to install https://github.com/nathants/cli-aws
+    exit 1
+fi
+
+id=$(aws-ec2-new --type z1d.large --ami arch py-webkit-test-box)
+trap "aws-ec2-rm -y $id" EXIT
+
+aws-ec2-ssh $id -yc '
+    curl https://raw.githubusercontent.com/nathants/py-webkit/master/scripts/install_archlinux.sh | bash
+    sudo pacman -S --noconfirm \
+         leiningen \
+         npm
+    if ! which runclj &>/dev/null; then (
+        rm -rf runclj
+        git clone https://github.com/nathants/runclj
+        sudo mv runclj/bin/* runclj/bin/.lein/ /usr/local/bin
+    ) fi
+'
+
+aws-ec2-ssh $id -yc '
+    git clone https://github.com/nathants/py-webkit
+    cd py-webkit
+    sudo python -m pip install pytest requests
+    xvfb-run -d -e error.log python example/test.py
+'
+
+aws-ec2-scp :/tmp/screen.png /tmp/ $id -y
---
+++ install_archlinux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eou pipefail
+sudo pacman -Sy --noconfirm \
+     git \
+     python-pip \
+     python-pyqt5 \
+     python-pyqtwebengine \
+     xorg-server-xvfb
+sudo python -m pip install git+https://github.com/nathants/py-webkit
